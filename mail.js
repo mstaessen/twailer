fe013ed0e41417cc160f425ab174161b8a371c68
Michiel Staessen <staessenmichiel@gmail.com>
2012-11-10 15:55:42 +0100

    Mail Receive script

---
+++ mail.js
@@ -0,0 +1,37 @@
+#!/usr/local/bin/node
+
+var fs         = require('fs')
+  , MailParser = require('mailparser').MailParser;
+
+var rawMail = '';
+var mp = new MailParser();
+mp.on('end', function(mail){
+  var action = mail.to[0].address.toLowerCase().substr(0, mail.to[0].address.indexOf('@'));
+  var track = mail.subject;
+  var subscriber = mail.from[0].address;
+  switch(action) {
+    case "subscribe":
+      console.log("Subscribe " + subscriber + " to " + track);
+      break;
+    case "unsubscribe":
+      console.log("Unsubscribe " + subscriber + " from " + track);
+      break;
+  }  
+
+
+fs.writeFile('/home/twailer/msg', rawMail, function(err) {
+    if(err) {
+      process.stderr.write('failed to write');
+    }
+  });  
+});
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+process.stdin.on('data', function(chunk) {
+  mp.write(chunk);
+  rawMail += chunk;
+});
+process.stdin.on('end', function () {
+  mp.end();
+});
